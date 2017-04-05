fd1d367d3d00402e3ff2f07f0f3fa85ec7ccf8e3
Aaron Perkins <perkinsa3@udayton.edu>
2017-04-05 19:13:39 -0400

    worked on report pothole javascript

--- Capstone.Web/Scripts/reportPothole.js
+++ Capstone.Web/Scripts/reportPothole.js
@@ -1,15 +1,35 @@
-﻿$(document).ready(function initMap() {
+﻿$(document).ready(function reportMap() {
 
     var cleveland = { lat: 41.505, lng: -81.681 };
     var map = new google.maps.Map(document.getElementById('map'), {
         zoom: 13,
         center: cleveland
     });
+    
+    var pothole;
 
-    var marker;
+    google.maps.event.addListener(map, 'click', function (event) {
+
+        if (pothole) {
+            pothole.window.close();
+            pothole.pin.setMap(null);
+        }
+
+        pothole = placeMarker(event.latLng);
+        google.maps.event.addListener(pothole.window, 'closeclick', function (event) {
+            pothole.pin.setMap(null);
+        });
+    });
+
+    var contentString = '<a href="/function/savepothole">Submit Pothole</a>';
+
+
     //var image = 'https://cdn2.iconfinder.com/data/icons/bad-day/64/flat_tire_unhappy_bad_luck_depression_fail_failure_defeat-512.png';
 
     function placeMarker(location) {
+
+        var marker;
+
         if (marker) {
             marker.setPosition(location);
         } else {
@@ -19,21 +39,23 @@
                 //icon: image
             });
         }
+
+        var infowindow = new google.maps.InfoWindow({
+            content: contentString
+        });
+
+        infowindow.open(map, marker);
+
+        return {
+            pin: marker,
+            window: infowindow
+        }
     }
 
-    var contentString = '<a href="/function/savepothole">Submit Pothole</a>'
-
-    var infowindow = new google.maps.InfoWindow({
-        content: contentString
-    });
-
-    google.maps.event.addListener(map, 'click', function (event) {
-        placeMarker(event.latLng);
-        infowindow.open(map, marker);
-    });
-
-    google.maps.event.addListener(infowindow, 'closeclick', function (event) {
-        marker.setMap(null);
-    });
-
+    //function openWindow() {
+    //    infowindow = new google.maps.InfoWindow({
+    //        content: contentString
+    //    });
+    //    infowindow.open(map, marker);
+    //}
 });
