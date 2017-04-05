0add65282717c893cf949d718f50e8cede201992
levineh@msn.com <levineh@msn.com>
2017-04-05 16:56:56 -0400

    added PotholeModel and PotholeSqlDAL

---
+++ Capstone.Web/DAL/IPotholeDAL.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Capstone.Web.Models;
+
+namespace Capstone.Web.DAL
+{
+    public interface IPotholeDAL
+    {
+        bool ReportPothole(PotholeModel newPothole);
+        List<PotholeModel> GetAllPotholes();
+
+    }
+}
---
+++ Capstone.Web/Models/PotholeModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Capstone.Web.Models
+{
+    public class PotholeModel
+    {
+        public int PotholeID { get; set; }
+        public double Longitude { get; set; }
+        public double Latitude { get; set; }
+        public int WhoReported { get; set; }
+        public int WhoInspected { get; set; }
+        public string Picture { get; set; }
+        public DateTime ReportDate { get; set; }
+        public DateTime InspectDate { get; set; }
+        public DateTime RepairStartDate { get; set; }
+        public DateTime RepairEndDate { get; set; }
+        public int Severity { get; set; }
+        public string Comment { get; set; }
+    }
+}
